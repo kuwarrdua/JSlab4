cc02312cb85a531413e954a8f304e7b26df1f721
kuwarrdua <55241176+kuwarrdua@users.noreply.github.com>
2020-03-26 13:41:07 -0400

    Add files via upload

---
+++ weird.js
@@ -0,0 +1,32 @@
+function useAsynchronous(url,callback){
+	let xhr = new XMLHttpRequest();
+ 	xhr.open('GET', url);
+ 	xhr.send();
+}
+
+function weirdOffers(jsonObj){
+	let weirdDeals = jsonObj.weirdDeals;
+	let section = document.querySelector('section');
+	for(let i=0; i<weirdDeals.length;i++){
+		let article = document.createElement('article');
+		let h2 = document.createElement('h2');
+		let img = document.createElement('img');
+		let p1 = document.createElement('p');
+		let p2 = document.createElement('p');
+		let ul = document.createElement('ul');
+
+		img.setAttribute('src', url + weirdDeals[i].image);
+		img.setAttribute('alt',weirdDeals[i].image);
+		h2.textContent = weirdDeals[i].name;
+		p1.textContent = 'Price '+ weirdDeals[i].price;
+		p2.textContent = 'Details '+ weirdDeals[i].details;
+
+		article.appendChild(img);
+		article.appendChild(h2);
+		article.appendChild(p1);
+		article.appendChild(p2);
+		article.appendChild(ul);
+		section.appendChild(article);
+
+	}
+}

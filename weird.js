487b555967bce1b6c2d013ce9e8ad36efb736bec
kuwarrdua <55241176+kuwarrdua@users.noreply.github.com>
2020-03-26 14:20:39 -0400

    Add files via upload

--- weird.js
+++ weird.js
@@ -1,26 +1,26 @@
 
+let url = 'https://kuwarrdua.github.io/JSlab4/weird.json';
+
 function useAsynchronous(url,callback){
 	let xhr = new XMLHttpRequest();
  	xhr.open('GET', url);
- 	xhr.send();
+ 	xhr.responseType= 'json';
  	xhr.onload = function(){
-	let weird = xhr.response;
-	console.log(weird);
-	weirdOffers(weird);
+ 		callback(xhr.response);
+ 	};
+ 	xhr.send();
 };
-}
 
 
-function weirdOffers(jsonObj){
+function weirdDeals(jsonObj){
 	let weirdDeals = jsonObj.weirdDeals;
 	let section = document.querySelector('section');
-	for(let i=0; i<weirdDeals.length;i++){
+	for(let i=0; i< weirdDeals.length;i++){
 		let article = document.createElement('article');
 		let h2 = document.createElement('h2');
 		let img = document.createElement('img');
 		let p1 = document.createElement('p');
 		let p2 = document.createElement('p');
-		let ul = document.createElement('ul');
 
 		img.setAttribute('src', 'https://kuwarrdua.github.io/JSlab4/images/' + weirdDeals[i].image);
 		img.setAttribute('alt',weirdDeals[i].image);
@@ -32,10 +32,9 @@ function weirdOffers(jsonObj){
 		article.appendChild(h2);
 		article.appendChild(p1);
 		article.appendChild(p2);
-		article.appendChild(ul);
 		section.appendChild(article);
 
 	}
 }
 
-useAsynchronous('https://kuwarrdua.github.io/JSlab4/weird.json',weirdOffers);
+useAsynchronous('https://kuwarrdua.github.io/JSlab4/weird.json',weirdDeals);
